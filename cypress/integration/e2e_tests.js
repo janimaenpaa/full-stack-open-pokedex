7617cc9f94c1b80d34c2795519b324f85203d758
janimaenpaa <itsmayd@gmail.com>
2021-02-24 12:30:52 +0200

    Edit cypress test

--- cypress/integration/e2e_tests.js
+++ cypress/integration/e2e_tests.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-undef */
 describe('Pokedex', function () {
-  it('front page can be opened', function () {
+  before(() => {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
   it('pokemon page can be navigated to', function () {
-    cy.visit('http://localhost:5000')
     cy.get('[href="/pokemon/ivysaur"]').click()
     cy.contains('overgrow')
     cy.contains('chlorophyll')
